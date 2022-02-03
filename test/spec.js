03118f9b441dad070dd4203d4072b711d2945cda
Caitlin Morrison Paulos <caitlinmorrison@sandiego.edu>
2022-02-02 18:17:31 -0800

    add-travis

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,16 @@
 const { expect } = require('chai')
+const app = require('supertest')(require('./app'))
 
 describe('my app', () => {
     beforeEach(() => syncAndSeed())
     it('true equals true', () => {
         expect(true).equals(true)
+    })
+    describe('routes', () => {
+        describe('GET /api/users', () => {
+            const response = await app.get('/api.users')
+            expect(response.status).to.equal(200)
+            expect(response.body.length).to.equal(2)
+        })
     })
 })
